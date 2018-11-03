4e35b72e83b42245f4bf2451087400cec70183fd
Jonathan <jb6330@gmail.com>
2018-11-03 00:45:59 -0400

    First implementation of parsing algorithm

--- Image Tagger/PictureViewer.cs
+++ Image Tagger/PictureViewer.cs
@@ -10,6 +10,16 @@ using System.Windows.Forms;
 
 namespace Image_Tagger
 {
+    enum OperatorTypes
+    {
+        NONE = 0,
+        AND,
+        OR,
+        NOT,
+        OpenBracket,
+        CloseBracket
+    }
+
     public partial class PictureViewer : Form
     {
         private Database database;
@@ -31,6 +41,68 @@ namespace Image_Tagger
                 ImageSelector.Controls.Add(pictureBox);
                 displayedPictures.Add(record, pictureBox);
             }
+        }
+
+        public static string[] ConvertToReversePolishNotation(string equation)
+        {
+            List<string> output = new List<string>();
+            Stack<OperatorTypes> operatorStack = new Stack<OperatorTypes>();
+
+            string cleanedString = equation;
+            cleanedString = cleanedString.Replace("(", " ( ");
+            cleanedString = cleanedString.Replace(")", " ) ");
+            string[] terms = cleanedString.Split(' ');
+
+            string newTerm = "";
+            foreach(string term in terms)
+            {
+                switch (term)
+                {
+                    case "(":
+                        if (newTerm != "") { throw new InvalidOperationException("( operater found directly following term"); }
+                        operatorStack.Push(OperatorTypes.OpenBracket);
+                        break;
+                    case ")":
+                        output.Add(newTerm.Trim());
+                        newTerm = "";
+                        while (operatorStack.Count > 0 && operatorStack.Peek() != OperatorTypes.OpenBracket)
+                        {
+                            output.Add(operatorStack.Pop().ToString());
+                        }
+                        operatorStack.Pop();
+                        break;
+                    case "NOT":
+                        if(newTerm != "") { throw new InvalidOperationException("NOT operater found directly following term"); }
+                        operatorStack.Push(OperatorTypes.NOT);
+                        break;
+                    case "AND":
+                        output.Add(newTerm.Trim());
+                        newTerm = "";
+                        while (operatorStack.Count > 0 && operatorStack.Peek() == OperatorTypes.NOT) { output.Add(operatorStack.Pop().ToString()); }
+                        operatorStack.Push(OperatorTypes.AND);
+                        break;
+                    case "OR":
+                        output.Add(newTerm.Trim());
+                        newTerm = "";
+                        while (operatorStack.Count > 0 && operatorStack.Peek() == OperatorTypes.NOT) { output.Add(operatorStack.Pop().ToString()); }
+                        operatorStack.Push(OperatorTypes.OR);
+                        break;
+                    case "":
+                        break;
+                    default:
+                        newTerm += " " + term;
+                        break;
+                }
+            }
+            if(newTerm != "")
+            {
+                output.Add(newTerm.Trim());
+            }
+            while (operatorStack.Count > 0)
+            {
+                output.Add(operatorStack.Pop().ToString());
+            }
+            return output.ToArray();
         }
     }
 }
