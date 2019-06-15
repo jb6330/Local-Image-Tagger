94b3a51c23ee57bbdecf5f2c76823be6e5bbd84c
Jonathan Barber <jb6330@gmail.com>
2019-06-15 18:18:31 -0400

    Moved search functionality to separate filter object. Search clears and re-adds instead of adjusting visibility of individual pictures.

--- Image Tagger/PictureViewer.cs
+++ Image Tagger/PictureViewer.cs
@@ -15,7 +15,7 @@
     public partial class PictureViewer : Form
     {
         private Database database;
-        private Dictionary<PictureRecord, PictureBox> displayedPictures;
+        private SearchFilter filter = new SearchFilter();
 
         /// <summary>Initializes a new instance of the <see cref="PictureViewer"/> class.</summary>
         public PictureViewer()
@@ -23,158 +23,10 @@
             this.InitializeComponent();
             this.database = new Database();
             this.database.LoadRecords(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.xml"));
-            this.displayedPictures = new Dictionary<PictureRecord, PictureBox>();
             foreach (var record in this.database.PictureRecords)
             {
                 this.AddRecord(record);
             }
-        }
-
-        /// <summary>Operators that can be used in the search equation.</summary>
-        private enum OperatorTypes
-        {
-            NONE = 0,
-            AND,
-            OR,
-            NOT,
-            OpenBracket,
-            CloseBracket,
-        }
-
-        /// <summary>Parses an equation into a list of terms and operators in Reverse Polish Notation.</summary>
-        /// <param name="equation">The equation to parse.</param>
-        /// <returns>The terms and operators in order of Reverse Polish Notation.</returns>
-        public static string[] ConvertToReversePolishNotation(string equation)
-        {
-            List<string> output = new List<string>();
-            Stack<OperatorTypes> operatorStack = new Stack<OperatorTypes>();
-
-            string cleanedString = equation;
-            cleanedString = cleanedString.Replace("(", " ( ");
-            cleanedString = cleanedString.Replace(")", " ) ");
-            string[] terms = cleanedString.Split(' ');
-
-            string newTerm = string.Empty;
-            foreach (string term in terms)
-            {
-                switch (term)
-                {
-                    case "(":
-                        if (newTerm != string.Empty)
-                        {
-                            throw new InvalidOperationException("( operater found directly following term");
-                        }
-
-                        operatorStack.Push(OperatorTypes.OpenBracket);
-                        break;
-                    case ")":
-                        if (newTerm != string.Empty)
-                        {
-                            output.Add(newTerm.Trim().ToLowerInvariant());
-                        }
-
-                        newTerm = string.Empty;
-                        while (operatorStack.Count > 0 && operatorStack.Peek() != OperatorTypes.OpenBracket)
-                        {
-                            output.Add(operatorStack.Pop().ToString());
-                        }
-
-                        operatorStack.Pop();
-                        break;
-                    case "NOT":
-                        if (newTerm != string.Empty)
-                        {
-                            throw new InvalidOperationException("NOT operater found directly following term");
-                        }
-
-                        operatorStack.Push(OperatorTypes.NOT);
-                        break;
-                    case "AND":
-                        if (newTerm != string.Empty)
-                        {
-                            output.Add(newTerm.Trim().ToLowerInvariant());
-                        }
-
-                        newTerm = string.Empty;
-                        while (operatorStack.Count > 0 && operatorStack.Peek() == OperatorTypes.NOT)
-                        {
-                            output.Add(operatorStack.Pop().ToString());
-                        }
-
-                        operatorStack.Push(OperatorTypes.AND);
-                        break;
-                    case "OR":
-                        if (newTerm != string.Empty)
-                        {
-                            output.Add(newTerm.Trim().ToLowerInvariant());
-                        }
-
-                        newTerm = string.Empty;
-                        while (operatorStack.Count > 0 && operatorStack.Peek() == OperatorTypes.NOT)
-                        {
-                            output.Add(operatorStack.Pop().ToString());
-                        }
-
-                        operatorStack.Push(OperatorTypes.OR);
-                        break;
-                    case "":
-                        break;
-                    default:
-                        newTerm += " " + term;
-                        break;
-                }
-            }
-
-            if (newTerm != string.Empty)
-            {
-                output.Add(newTerm.Trim().ToLowerInvariant());
-            }
-
-            while (operatorStack.Count > 0)
-            {
-                output.Add(operatorStack.Pop().ToString());
-            }
-
-            return output.ToArray();
-        }
-
-        /// <summary>Evaluate the equation on against a list of tags.</summary>
-        /// <param name="equation">The search equation to evaluate.</param>
-        /// <param name="tags">The list of tags that the image has.</param>
-        /// <returns>Whether or not the tags satisfy the search equation.</returns>
-        public bool SolveEquation(string[] equation, HashSet<string> tags)
-        {
-            Stack<bool> buffer = new Stack<bool>();
-            bool operand1, operand2;
-            foreach (string entry in equation)
-            {
-                switch (entry)
-                {
-                    case "NOT":
-                        buffer.Push(!buffer.Pop());
-                        break;
-                    case "AND":
-                        operand1 = buffer.Pop();
-                        operand2 = buffer.Pop();
-                        buffer.Push(operand1 && operand2);
-                        break;
-                    case "OR":
-                        operand1 = buffer.Pop();
-                        operand2 = buffer.Pop();
-                        buffer.Push(operand1 || operand2);
-                        break;
-                    default:
-                        buffer.Push(tags.Contains(entry));
-                        break;
-                }
-            }
-
-            if (buffer.Count != 1)
-            {
-                throw new ArgumentException("Invalid equation");
-            }
-
-            return buffer.Pop();
         }
 
         private void AddRecord(PictureRecord record)
@@ -186,33 +38,28 @@
                 Size = new Size(300, 300),
             };
             this.ImageSelector.Controls.Add(pictureBox);
-            this.displayedPictures.Add(record, pictureBox);
         }
 
         // TODO: split this off into a separate thread
         private void Search(string text)
         {
-            if (text.Trim() == string.Empty)
+            this.ImageSelector.Controls.Clear();
+            text = text.Trim();
+
+            if (text == string.Empty)
             {
                 foreach (var entry in this.database.PictureRecords)
                 {
-                    this.displayedPictures[entry].Invoke((Action)(() => this.displayedPictures[entry].Visible = true));
+                    this.AddRecord(entry);
                 }
             }
-
-            string[] equation = ConvertToReversePolishNotation(text);
-            foreach (var entry in this.database.PictureRecords)
+            else
             {
-                bool displayed = this.SolveEquation(equation, entry.Tags);
-
-                this.displayedPictures[entry].Invoke((Action)(
-                    () =>
-                    {
-                        if (this.displayedPictures[entry].Visible != displayed)
-                        {
-                            this.displayedPictures[entry].Visible = displayed;
-                        }
-                    }));
+                var records = this.filter.CreateFilteredList(this.database.PictureRecords.ToList(), text);
+                foreach (var entry in records)
+                {
+                    this.AddRecord(entry);
+                }
             }
         }
 
@@ -254,7 +101,6 @@
             if (dialog.ShowDialog() == DialogResult.OK && dialog.SelectedPath.Trim() != string.Empty)
             {
                 this.database.AddFolder(dialog.SelectedPath);
-                this.displayedPictures.Clear();
                 this.ImageSelector.Controls.Clear();
                 foreach (var record in this.database.PictureRecords)
                 {
---
+++ Image Tagger/SearchFilter.cs
@@ -0,0 +1,179 @@
+﻿namespace Image_Tagger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>A filter that returns records whose tags match the search equation.</summary>
+    internal class SearchFilter
+    {
+        /// <summary>Operators that can be used in the search equation.</summary>
+        private enum OperatorTypes
+        {
+            NONE = 0,
+            AND,
+            OR,
+            NOT,
+            OpenBracket,
+            CloseBracket,
+        }
+
+        /// <summary>Filter a list of records based on a search equation.</summary>
+        /// <param name="pictures">The list of records.</param>
+        /// <param name="equation">An equation that the desired records must match.</param>
+        /// <returns>A list of matching records.</returns>
+        public List<PictureRecord> CreateFilteredList(List<PictureRecord> pictures, string equation)
+        {
+            string[] parsedEquation = ConvertToReversePolishNotation(equation);
+            var output = new List<PictureRecord>();
+            foreach (PictureRecord record in pictures)
+            {
+                if (this.SolveEquation(parsedEquation, record.Tags))
+                {
+                    output.Add(record);
+                }
+            }
+
+            return output;
+        }
+
+        /// <summary>Parses an equation into a list of terms and operators in Reverse Polish Notation.</summary>
+        /// <param name="equation">The equation to parse.</param>
+        /// <returns>The terms and operators in order of Reverse Polish Notation.</returns>
+        private static string[] ConvertToReversePolishNotation(string equation)
+        {
+            List<string> output = new List<string>();
+            Stack<OperatorTypes> operatorStack = new Stack<OperatorTypes>();
+
+            string cleanedString = equation;
+            cleanedString = cleanedString.Replace("(", " ( ");
+            cleanedString = cleanedString.Replace(")", " ) ");
+            string[] terms = cleanedString.Split(' ');
+
+            string newTerm = string.Empty;
+            foreach (string term in terms)
+            {
+                switch (term)
+                {
+                    case "(":
+                        if (newTerm != string.Empty)
+                        {
+                            throw new InvalidOperationException("( operater found directly following term");
+                        }
+
+                        operatorStack.Push(OperatorTypes.OpenBracket);
+                        break;
+                    case ")":
+                        if (newTerm != string.Empty)
+                        {
+                            output.Add(newTerm.Trim().ToLowerInvariant());
+                        }
+
+                        newTerm = string.Empty;
+                        while (operatorStack.Count > 0 && operatorStack.Peek() != OperatorTypes.OpenBracket)
+                        {
+                            output.Add(operatorStack.Pop().ToString());
+                        }
+
+                        operatorStack.Pop();
+                        break;
+                    case "NOT":
+                        if (newTerm != string.Empty)
+                        {
+                            throw new InvalidOperationException("NOT operater found directly following term");
+                        }
+
+                        operatorStack.Push(OperatorTypes.NOT);
+                        break;
+                    case "AND":
+                        if (newTerm != string.Empty)
+                        {
+                            output.Add(newTerm.Trim().ToLowerInvariant());
+                        }
+
+                        newTerm = string.Empty;
+                        while (operatorStack.Count > 0 && operatorStack.Peek() == OperatorTypes.NOT)
+                        {
+                            output.Add(operatorStack.Pop().ToString());
+                        }
+
+                        operatorStack.Push(OperatorTypes.AND);
+                        break;
+                    case "OR":
+                        if (newTerm != string.Empty)
+                        {
+                            output.Add(newTerm.Trim().ToLowerInvariant());
+                        }
+
+                        newTerm = string.Empty;
+                        while (operatorStack.Count > 0 && operatorStack.Peek() == OperatorTypes.NOT)
+                        {
+                            output.Add(operatorStack.Pop().ToString());
+                        }
+
+                        operatorStack.Push(OperatorTypes.OR);
+                        break;
+                    case "":
+                        break;
+                    default:
+                        newTerm += " " + term;
+                        break;
+                }
+            }
+
+            if (newTerm != string.Empty)
+            {
+                output.Add(newTerm.Trim().ToLowerInvariant());
+            }
+
+            while (operatorStack.Count > 0)
+            {
+                output.Add(operatorStack.Pop().ToString());
+            }
+
+            return output.ToArray();
+        }
+
+        /// <summary>Evaluate the equation on against a list of tags.</summary>
+        /// <param name="equation">The search equation to evaluate.</param>
+        /// <param name="tags">The list of tags that the image has.</param>
+        /// <returns>Whether or not the tags satisfy the search equation.</returns>
+        private bool SolveEquation(string[] equation, HashSet<string> tags)
+        {
+            Stack<bool> buffer = new Stack<bool>();
+            bool operand1, operand2;
+            foreach (string entry in equation)
+            {
+                switch (entry)
+                {
+                    case "NOT":
+                        buffer.Push(!buffer.Pop());
+                        break;
+                    case "AND":
+                        operand1 = buffer.Pop();
+                        operand2 = buffer.Pop();
+                        buffer.Push(operand1 && operand2);
+                        break;
+                    case "OR":
+                        operand1 = buffer.Pop();
+                        operand2 = buffer.Pop();
+                        buffer.Push(operand1 || operand2);
+                        break;
+                    default:
+                        buffer.Push(tags.Contains(entry));
+                        break;
+                }
+            }
+
+            if (buffer.Count != 1)
+            {
+                throw new ArgumentException("Invalid equation");
+            }
+
+            return buffer.Pop();
+        }
+    }
+}
